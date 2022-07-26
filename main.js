47f16a2e8740b20f2b2efd9d79d7b5685e174ef7
dixk3458 <dixk3458@naver.com>
2022-07-26 22:37:04 +0900

    JS:Home transparent

--- main.js
+++ main.js
@@ -31,6 +31,15 @@ homeContactBtn.addEventListener('click', () => {
   ScrollIntoView('#contact');
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function ScrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
